9c4d0c611f13e581e6fc2888bfbc97f17d65fa85
Shehroz Amjad <shehrozaaa@gmail.com>
2024-07-26 00:51:11 +0500

    Using the Map and MapWhen Methods

--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -46,6 +46,23 @@ app.Use(async (context,next) =>
     Console.WriteLine("Code execute After jump to back from next delegate in the Use method");
 });
 
+app.Map("/usingmapbranch", builder =>
+{
+    builder.Use(async (context, next) =>
+    {
+        Console.WriteLine("Map branch logic in the Use method before the next delegate");
+        await next.Invoke();
+        Console.WriteLine("Map branch logic in the Use method after the next delegate");
+    });
+
+    builder.Run(async context =>
+    {
+        Console.WriteLine($"Map branch response to the client in the Run method");
+        await context.Response.WriteAsync("Hello from the map branch.");
+    });
+});
+
+
 app.Run(async context =>
 {
     Console.WriteLine($"Writing the response to the client in the Run method");
