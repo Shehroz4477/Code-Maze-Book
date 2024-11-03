84490ef7f369092da4f189afa159f3c4fedd0225
shehroz4477 <shehrozaaa@Gmail.com>
2024-11-04 04:06:22 +0500

    Context Class and Database Connection

---
+++ Repository/RepositoryContext.cs
@@ -0,0 +1,14 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repository;
+
+public class RepositoryContext : DbContext
+{
+    public RepositoryContext(DbContextOptions options) : base(options)
+    {
+        //TODO
+    }
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<Company> Companies { get; set; }
+}
