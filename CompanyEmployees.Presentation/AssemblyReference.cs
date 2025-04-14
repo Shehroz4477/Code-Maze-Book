53d865255c14b953f793780d6d02a28abb0194d3
Shehroz <shehroz.amjad@arabsea.com>
2025-04-14 11:13:57 +0500

    Controller and Routing in WEB API

---
+++ CompanyEmployees.Presentation/AssemblyReference.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.Xml;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyEmployees.Presentation;
+/// <summary>
+/// It's an empty static class that we are going to use for the assembly reference inside the main project.
+/// </summary>
+public static class AssemblyReference
+{
+}
---
+++ CompanyEmployees.Presentation/Controllers/CompaniesController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CompanyEmployees.Presentation.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class CompaniesController:ControllerBase
+{
+}
