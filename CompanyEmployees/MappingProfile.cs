60b101d53d4c161f4f163a2acbb99aed0996cdfc
Shehroz <shehroz.amjad@arabsea.com>
2025-04-16 09:08:18 +0500

    Using AutoMapper in ASP.NET Core : Create MappingProfile Class

---
+++ CompanyEmployees/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Entities.Models;
+using Shared.DataTransferObjects;
+
+namespace CompanyEmployees;
+
+public class MappingProfile:Profile
+{
+    public MappingProfile() 
+    {
+        CreateMap<Company, CompanyDto>()
+            .ForMember<string>
+            (
+                cmpDto => cmpDto.FullAddress,
+                opt => opt.MapFrom<string>(cmp => string.Join(' ', cmp.Address, cmp.Country))
+            );
+    }
+}
