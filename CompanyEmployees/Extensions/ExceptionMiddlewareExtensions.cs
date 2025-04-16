a3f01e565f49e23fb5fdf055640d439209c63750
Shehroz <shehroz.amjad@arabsea.com>
2025-04-16 19:11:14 +0500

    Handling Errors Globally with the Build-In Middleware

---
+++ CompanyEmployees/Extensions/ExceptionMiddlewareExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using Contract.Interfaces;
+using Entities.ErrorModel;
+using Microsoft.AspNetCore.Diagnostics;
+
+namespace CompanyEmployees.Extensions;
+
+public static class ExceptionMiddlewareExtensions
+{
+    public static void ConfigureExceptionHandler(this WebApplication app, ILoggerManager logger)
+    {
+        app.UseExceptionHandler(appError =>
+        {
+            appError.Run(async context =>
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                context.Response.ContentType = "application/json";
+
+                var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                if(contextFeature != null)
+                {
+                    logger.LogError($"Somthing went wrong {contextFeature.Error}");
+
+                    await context.Response.WriteAsync(new ErrorDetails()
+                    {
+                        SttausCode = context.Response.StatusCode,
+                        Message = "Internal Server Error."
+                    }.ToString());
+                }
+            });
+        });
+    }
+}
