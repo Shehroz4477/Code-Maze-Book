6085b36ec8dc4b0a19203ecccfad43dfef4d1caa
Shehroz <shehroz.amjad@arabsea.com>
2025-04-24 20:53:37 +0500

    Creation a children Resources Together with a Parent

--- Shared/DataTransferObjects/CompanyForCreationDto.cs
+++ Shared/DataTransferObjects/CompanyForCreationDto.cs
@@ -6,4 +6,4 @@ using System.Threading.Tasks;
 
 namespace Shared.DataTransferObjects;
 
-public record CompanyForCreationDto(string Name, string Address, string Country);
+public record CompanyForCreationDto(string Name, string Address, string Country, IEnumerable<EmployeeForCreationDto> Employees);
