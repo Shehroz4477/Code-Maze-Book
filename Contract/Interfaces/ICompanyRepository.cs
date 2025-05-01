6ea54b132329ae98f098c76ecef69b97f365873d
Shehroz <shehroz.amjad@arabsea.com>
2025-05-01 09:32:19 +0500

    Company Repository changes Asynchronously

--- Contract/Interfaces/ICompanyRepository.cs
+++ Contract/Interfaces/ICompanyRepository.cs
@@ -9,9 +9,9 @@ namespace Contract.Interfaces;
 
 public interface ICompanyRepository
 {
-    IEnumerable<Company> GetAllCompanies(bool trackChanges);
-    Company GetCompany(Guid companyId, bool trackChanges);
+    Task<IEnumerable<Company>> GetAllCompanies(bool trackChanges);
+    Task<Company> GetCompany(Guid companyId, bool trackChanges);
     void CreateCompany(Company company);
-    IEnumerable<Company> GetByIds(IEnumerable<Guid> ids, bool trackChanges);
+    Task<IEnumerable<Company>> GetByIds(IEnumerable<Guid> ids, bool trackChanges);
     void DeleteCompany(Company company);
 }
