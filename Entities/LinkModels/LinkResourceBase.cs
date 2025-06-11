d0c55d4e31925c65c91c5bed01b397d781a98e93
Shehroz <shehroz.amjad@arabsea.com>
2025-06-11 14:36:02 +0500

    We need to create a class that will contain all of our links — LinkResourceBase

---
+++ Entities/LinkModels/LinkResourceBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.LinkModels;
+
+internal class LinkResourceBase
+{
+    public LinkResourceBase()
+    {
+        //TODO    
+    }
+       
+    public List<Link> Links { get; set; } = new List<Link>();
+}
