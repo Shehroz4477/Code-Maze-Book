0aeabfac695d70de4133eff88cec4e5479f73067
Shehroz <shehroz.amjad@arabsea.com>
2025-06-18 18:09:04 +0500

    modify the IServiceManager and ServieManager for auth service

--- Service/ServiceManager.cs
+++ Service/ServiceManager.cs
@@ -5,6 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
 using Contract.Interfaces;
+using Entities.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
 using Service.Contracts.Interfaces;
 using Shared.DataTransferObjects;
 
@@ -14,12 +17,15 @@ namespace Service
     {
         private readonly Lazy<ICompanyService> _companyService;
         private readonly Lazy<IEmployeeService> _employeeService;
-        public ServiceManager(IRepositoryManager repository, ILoggerManager logger, IMapper mapper, IDataShaper<EmployeeDto> dataShaper) 
+        private readonly Lazy<IAuthenticationService> _authenticationService
+        public ServiceManager(IRepositoryManager repository, ILoggerManager logger, IMapper mapper, IDataShaper<EmployeeDto> dataShaper, UserManager<User> userManager, IConfiguration configuration) 
         {
             _companyService = new Lazy<ICompanyService>(() => new CompanyService(repository,logger,mapper));
             _employeeService = new Lazy<IEmployeeService>(() => new EmployeeService(repository,logger,mapper,dataShaper));
+            _authenticationService = new Lazy<IAuthenticationService>(() => new AuthenticationService(logger,mapper,userManager,configuration));
         }
         public ICompanyService CompanyService => _companyService.Value;
         public IEmployeeService EmployeeService => _employeeService.Value;
+        public IAuthenticationService AuthenticationService => _authenticationService.Value;
     }
 }
