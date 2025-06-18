24872756ec97c994947b6961cac7ddebb2cee5a6
Shehroz <shehroz.amjad@arabsea.com>
2025-06-18 17:59:45 +0500

    Authentication Service Define

---
+++ Service.Contracts/Interfaces/IAuthenticationService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Shared.DataTransferObjects;
+
+namespace Service.Contracts.Interfaces;
+
+public interface IAuthenticationService
+{
+    Task<IdentityResult> RegisterUser(UserForRegistrationDto userForRegistrationDto);
+}
---
+++ Service/AuthenticationService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Contract.Interfaces;
+using Entities.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Service.Contracts.Interfaces;
+using Shared.DataTransferObjects;
+
+namespace Service;
+
+internal sealed class AuthenticationService : IAuthenticationService
+{
+    private readonly ILoggerManager _logger;
+    private readonly IMapper _mapper;
+    private readonly UserManager<User> _userManager;
+    private readonly IConfiguration _configuration;
+
+    public AuthenticationService(ILoggerManager logger, IMapper mapper, UserManager<User> userManager, IConfiguration configuration)
+    {
+        _logger = logger;
+        _mapper = mapper;
+        _userManager = userManager;
+        _configuration = configuration;
+    }
+    public async Task<IdentityResult> RegisterUser(UserForRegistrationDto userForRegistrationDto)
+    {
+        var user = _mapper.Map<User>(userForRegistrationDto);
+
+        var result = await _userManager.CreateAsync(user, userForRegistrationDto.Password);
+
+        if(result.Succeeded)
+        {
+            await _userManager.AddToRolesAsync(user, userForRegistrationDto.Roles);
+        }
+
+        return result;
+    }
+}
