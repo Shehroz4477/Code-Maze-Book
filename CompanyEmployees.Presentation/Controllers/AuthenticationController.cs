efea762cc0b56e02999281787ca7920bb9989e3b
Shehroz <shehroz.amjad@arabsea.com>
2025-06-18 17:13:22 +0500

    define authentication controller

---
+++ CompanyEmployees.Presentation/Controllers/AuthenticationController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Service.Contracts.Interfaces;
+
+namespace CompanyEmployees.Presentation.Controllers;
+
+[Route("api/authentication")]
+[ApiController]
+public class AuthenticationController:ControllerBase
+{
+    private readonly IServiceManager _service;
+
+    public AuthenticationController(IServiceManager service)
+    {
+        _service = service;
+    }
+}
