57f07086acf0134610c793e7d7cad6bd45b8fb7b
Shehroz <shehroz.amjad@arabsea.com>
2025-06-18 18:17:09 +0500

    create the RegisterUser action

--- CompanyEmployees.Presentation/Controllers/AuthenticationController.cs
+++ CompanyEmployees.Presentation/Controllers/AuthenticationController.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CompanyEmployees.Presentation.ActionFilters;
 using Microsoft.AspNetCore.Mvc;
 using Service.Contracts.Interfaces;
+using Shared.DataTransferObjects;
 
 namespace CompanyEmployees.Presentation.Controllers;
 
@@ -17,5 +19,23 @@ public class AuthenticationController:ControllerBase
     public AuthenticationController(IServiceManager service)
     {
         _service = service;
+    }
+
+    [HttpPost]
+    [ServiceFilter(typeof(ValidationFilterAttribute))]
+    public async Task<IActionResult> RegisterUser([FromBody] UserForRegistrationDto userForRegistrationDto)
+    {
+        var result = await _service.AuthenticationService.RegisterUser(userForRegistrationDto);
+
+        if(!result.Succeeded)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.TryAddModelError(error.Code, error.Description);
+            }
+            return BadRequest(ModelState);
+        }
+
+        return StatusCode(201);
     }
 }
