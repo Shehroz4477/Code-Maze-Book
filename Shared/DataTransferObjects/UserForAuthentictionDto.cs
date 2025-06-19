fc78773ef7d359619b235e467bb27d22e58a598d
Shehroz <shehroz.amjad@arabsea.com>
2025-06-19 12:36:11 +0500

    UserForAuthenticationDto record define

---
+++ Shared/DataTransferObjects/UserForAuthentictionDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.DataTransferObjects;
+
+public record UserForAuthentictionDto
+{
+    [Required(ErrorMessage = "User name is required")]
+    public string UserName { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Password is required")]
+    public string Password { get; set; } = string.Empty;
+}
