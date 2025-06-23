5789d5880877cf76a20f47f6780059fc7714c046
Shehroz <shehroz.amjad@arabsea.com>
2025-06-23 10:55:55 +0500

    creating a new TokenController in the Presentation project

---
+++ CompanyEmployees.Presentation/Controllers/TokenController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Service.Contracts.Interfaces;
+
+namespace CompanyEmployees.Presentation.Controllers;
+
+[Route("api/token")]
+[ApiController]
+public class TokenController:ControllerBase
+{
+    private readonly IServiceManager _service;
+
+    public TokenController(IServiceManager service)
+    {
+        _service = service;
+    }
+}
