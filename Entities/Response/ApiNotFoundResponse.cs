1af29739a2957b2863fc4d3f7107fe35c1595d5d
Shehroz <shehroz.amjad@arabsea.com>
2025-06-25 09:38:55 +0500

    api not found response

---
+++ Entities/Response/ApiNotFoundResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Response;
+
+public abstract class ApiNotFoundResponse: ApiBaseResponse
+{
+    public string Message { get; set; } = string.Empty;
+
+    protected ApiNotFoundResponse(string message):base(false)
+    {
+        Message = message;
+    }
+}
