89e08fe33eaf570c5bec38aa0b789394e1090f7f
Shehroz <shehroz.amjad@arabsea.com>
2025-06-25 09:41:15 +0500

    api bad response

---
+++ Entities/Response/ApiBadRequestResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Response;
+
+public abstract class ApiBadRequestResponse: ApiBaseResponse
+{
+    public string Message { get; set; } = string.Empty;
+
+    protected ApiBadRequestResponse(string message):base(false)
+    {
+        Message = message;
+    }
+}
