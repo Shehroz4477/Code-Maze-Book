4b3dc211c45fc3a382b1d768653264177b457885
Shehroz <shehroz.amjad@arabsea.com>
2025-06-25 11:06:36 +0500

    ApiBaseResponse class uses in company service

--- Service.Contracts/Interfaces/ICompanyService.cs
+++ Service.Contracts/Interfaces/ICompanyService.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Entities.Response;
 using Shared.DataTransferObjects;
 
 namespace Service.Contracts.Interfaces;
 
 public interface ICompanyService
 {
-    Task<IEnumerable<CompanyDto>> GetAllCompaniesAsync(bool trackChanges);
-    Task<CompanyDto> GetCompanyAsync(Guid companyId,  bool trackChanges);
+    Task<ApiBaseResponse> GetAllCompaniesAsync(bool trackChanges);
+    Task<ApiBaseResponse> GetCompanyAsync(Guid companyId,  bool trackChanges);
     Task<CompanyDto> CreateCompanyAsync(CompanyForCreationDto company);
     Task<IEnumerable<CompanyDto>> GetByIdsAsync(IEnumerable<Guid> ids, bool trackChanges);
     Task<(IEnumerable<CompanyDto> companies, string ids)> CreateCompanyCollectionAsync(IEnumerable<CompanyForCreationDto> companyCollection);
